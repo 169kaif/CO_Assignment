c2b754afb8a389741157b5061f1817b49eca3774
169kaif <kaifraushan@gmail.com>
2022-07-01 23:46:20 +0530

    Upload python file and text file

---
+++ CO_assignment.py
@@ -0,0 +1,14 @@
+with open("CO_instructions.txt", "r") as f:
+	for line in f:
+		if (line=="\n"):
+			pass
+        if line == label
+
+        if line == instruction
+
+        if line == variable definition
+
+
+l_instructions = []
+l_variables = []
+l_labels =[]			
