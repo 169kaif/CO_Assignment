c94fe9b76c1ff0a55a4b926795a654c314c3e44c
169kaif <kaifraushan@gmail.com>
2022-07-02 00:42:49 +0530

    Pavit instruction dictionary

--- CO_assignment.py
+++ CO_assignment.py
@@ -6,10 +6,17 @@ with open("CO_instructions.txt", "r") as f:
 
 d_registers = {'R0':'000', 'R1':'001', 'R2':'010', 'R3':'011', 'R4':'100', 'R5':'101', 'R6':'110', 'FLAGS':'111'}
 
-correct_instructions = [x]
+correct_instructions = []
+faulty_instructions = []
 
 emptylines = 0
 
 for instruction in assembly_instructions:
 	if instruction == '\n':
 		emptylines+=1
+
+varlines=0
+
+for instruction in assembly_instructions:
+	if (instruction[0:3]=='var'):
+		varlines += 1
