44e977a3f5bfedf924599c2088331be97e839418
pait0709 <pavit21178@iiitd.ac.in>
2022-07-03 16:30:55 +0530

    driver

--- CO_assignment.py
+++ CO_assignment.py
@@ -39,7 +39,14 @@ d={"add":["A","10000"],"sub":["A","10001"],"mov":["B","10010"],"ld":["D","10100"
   "mul":["A","10110"],"div":["C","10111"],"rs":["B","11000"],"ls":["B","11001"],"xor":["A","11010"],"or":["A","11011"],
   "and":["A","11100"],"not":["C","11101"],"cmp":["C","11110"],"jmp":["E","11111"],"jlt":["E","01100"],"jgt":["E","01101"],
   "je":["E","01111"],"hlt":["F","01010"]}
+#label implementation
   
+d_labels = {}
+
+for index,line in enumerate(assembly_instructions):
+	ls = line.split()
+	if ((ls[0][-1] == ':') and ((ls[0][:len(ls[0])-1])not in d_labels)):
+		d_labels[(ls[0][:len(ls[0])-1])] = format(index - varlines, '08b')	
   #mov is in both "c" and "b"		
 
   #testcommit pavit
@@ -48,29 +55,35 @@ d={"add":["A","10000"],"sub":["A","10001"],"mov":["B","10010"],"ld":["D","10100"
 
   #label check niggers
 def f_A(a):
+	print(d[a[0]][1], end='')
 	reg1 = d_registers[a[1]]
 	reg2 = d_registers[a[2]]
 	reg3 = d_registers[a[3]]
 	print('00' + reg1 + reg2 + reg3)
 
 def f_B(a):
+	print(d[a[0]][1], end='')
 	reg1 = d_registers[a[1]]
 	print(reg1 + f'{int(a[2]):08b}')
 
 def f_C(a):
+	print(d[a[0]][1], end='')
 	reg1 = d_registers[a[1]]
 	reg2 = d_registers[a[2]]
 	print('00000' + reg1 + reg2 )
 
 def f_D(a):
+	print(d[a[0]][1], end='')
 	reg1 = d_registers[a[1]]
 	reg2 = d_var[a[2]]
 	print(reg1 + reg2)
 
 def f_E(a): #label implementation  here <-----
+	print(d[a[0]][1], end='')
 	reg1 = d_labels[a[1]]
 	print('000'+reg1)
-
+def f_F(a):
+	print(d[a[0]][1]+"00000000000")
 
 for (index,line) in assembly_instructions:
 	a = line.split()
@@ -79,28 +92,17 @@ for (index,line) in assembly_instructions:
 
 	if a[0] in d.keys():
 		if d[a[0]][0] == 'A':
-			print(d[a[0]][1], end='')
 			f_A(a)
 		if d[a[0]][0] == 'B':
-			print(d[a[0]][1], end='')
 			f_B(a)
 		if d[a[0]][0] == 'C':
-			print(d[a[0]][1], end='')
 			f_C(a)
 		if d[a[0]][0] == 'D':
-			print(d[a[0]][1], end='')
 			f_D(a)
 		if d[a[0]][0] == 'E': #label implementation here<-----
-			print(d[a[0]][1], end='')
 			f_E(a)
 		if d[a[0]][0] == 'F':
-			print(d[a[0]][1]+"00000000000")
+			f_F(a)
 
 #label implementation
 
-d_labels = {}
-
-for index,line in enumerate(assembly_instructions):
-	ls = line.split()
-	if ((ls[0][-1] == ':') and ((ls[0][:len(ls[0])-1])not in d_labels)):
-		d_labels[(ls[0][:len(ls[0])-1])] = format(index - varlines, '08b')	
