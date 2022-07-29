143a9002633b7095802d7c9e4a4f379c12ec722d
pait0709 <pavit21178@iiitd.ac.in>
2022-07-29 23:56:31 +0530

    hh

--- simulator.py
+++ simulator.py
@@ -1,9 +1,12 @@
-instructions=[]
+memory=[]
 with open("instructions.txt",'r') as f:
-    instructions=f.readlines
-def add():
+    memory=f.readlines()
+regs=[]
+def add(a,b,c):
+  regs[a]=regs[b]+regs[c]
 def sub():
-def mov():
+def movB():
+def movC():
 def ld():
 def st():
 def mul():
@@ -20,13 +23,12 @@ def jlt():
 def jgt():
 def je():
 def hlt():
-
-
-
-d={"add":["A","10000"],"sub":["A","10001"],"mov":["B","10010"],"ld":["D","10100"],"st":["D","10101"],
+d={"10000":"A","10001":"A","10010":"B","ld":["D","10100"],"st":["D","10101"],
   "mul":["A","10110"],"div":["C","10111"],"rs":["B","11000"],"ls":["B","11001"],"xor":["A","11010"],"or":["A","11011"],
   "and":["A","11100"],"not":["C","11101"],"cmp":["C","11110"],"jmp":["E","11111"],"jlt":["E","01100"],"jgt":["E","01101"],
-  "je":["E","01111"],"hlt":["F","01010"]}  
-for i in range(0,len(instructions)):
+  "je":["E","01111"],"hlt":["F","01010"]}
+
+h=len(memory)
+for i in range(0,h):
     opcode=instructions[:5]
     if 
